e4c76efa5be661e71b603999724753fdbb181668
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-26 07:37:25 +0530

    Add basic outline

---
+++ src/client.cpp
@@ -0,0 +1,24 @@
+/* client.cpp */
+
+#include <iostream>
+#include "util.h"
+
+using namespace std;
+
+char * interface = 0;
+
+int main(int argc, char ** argv) {
+  cout << "WiFi Math Client\n"
+          "----------------\tBy Jay H. Bosamiya\n"
+          "                \t------------------\n\n";
+
+  int ret = handle_params(argc,argv);
+
+  if ( ret != 0 ) {
+    return ret;
+  }
+
+  // TODO: Actual work
+
+  return 0;
+}
