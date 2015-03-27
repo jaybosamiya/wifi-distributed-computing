4f04b129914440b22711dd375ee7f9c347048eef
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-27 06:18:18 +0530

    Add packet definitions

---
+++ src/math_packet.cpp
@@ -0,0 +1,13 @@
+/* math_packet.cpp */
+
+// Define ways build and use a math packet
+
+#include "math_packet.h"
+
+std::pair<u_char*,int> make_packet_from_expression(std::string math_expression) {
+	// TODO
+}
+
+std::pair<u_char*,int> make_answer_packet(u_char* request_packet) {
+	// TODO
+}
