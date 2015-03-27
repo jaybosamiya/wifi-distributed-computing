fdd49546f87e1c4fa6a69fdd272cb62c383ac022
Jay Bosamiya <jaybosamiya@gmail.com>
2015-03-27 18:24:02 +0530

    Add client code

--- src/client.cpp
+++ src/client.cpp
@@ -1,8 +1,10 @@
 /* client.cpp */
 
 #include <iostream>
+#include <unistd.h>
 #include "util.h"
 #include "pcap_manager.h"
+#include "math_packet.h"
 
 using namespace std;
 
@@ -18,7 +20,60 @@ int main(int argc, char ** argv) {
   }
 
   initialize();
-  // TODO: Actual work
+
+  while ( true ) {
+    pcap_pkthdr hdr;
+    u_char* packet = const_cast<u_char*> (pcap_next(handle,&hdr));
+    int length = hdr.len;
+    Packet p;
+    p.first = packet;
+    p.second = length;
+    p = unwrap_datalink(p);
+
+    MathPacketHeader *mph = (MathPacketHeader *)p.first;
+    if ( mph->magic_number != MATH_MAGIC ) {
+      continue;
+    }
+
+    if ( mph->type_of_packet != MATH_TYPE_REQUEST ) {
+      continue;
+    }
+
+    Packet answer = make_answer_packet(p.first);
+    make_ack_packet(p);
+
+    pcap_sendpacket(handle,p.first,p.second);
+
+    while ( true ) {
+      pcap_sendpacket(handle,answer.first,answer.second);
+
+      packet = const_cast<u_char*> (pcap_next(handle,&hdr));
+      length = hdr.len;
+
+      p.first = packet;
+      p.second = length;
+      p = unwrap_datalink(p);
+
+      MathPacketHeader* mph2 = (MathPacketHeader *)p.first;
+      if ( mph2->magic_number != MATH_MAGIC ) {
+        continue;
+      }
+
+      if ( mph2->type_of_packet != MATH_TYPE_ACK_ANSWER ) {
+        continue;
+      }
+
+      if ( mph2->user_id_of_requester != mph->user_id_of_requester ) {
+        continue;
+      }
+
+      if ( mph2->request_id != mph2->request_id ) {
+        continue;
+      }
+
+      break;
+    }
+  }
 
   return 0;
 }
